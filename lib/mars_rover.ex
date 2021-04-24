af8730c70be0c0e6f9d0876fa4cf274413424df4
Himalee <himalee.tailor@gmail.com>
2021-04-24 14:02:10 +0100

    Return 'LOST' if robots final state is off the grid

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,7 +1,16 @@
 defmodule MarsRover do
 
-  def update_position([x_coordinate, y_coordinate, orientation], commands) do
-    commands = String.split(commands, "", trim: true)
+  def update_position([x_coordinate, y_coordinate, orientation], movement_commands, x_axis, y_axis) do
+    [final_x_coordinate, final_y_coordinate, final_orientation] = final_state([x_coordinate, y_coordinate, orientation], movement_commands)
+    if off_grid?(final_x_coordinate, final_y_coordinate, x_axis, y_axis) do
+      "LOST"
+    else
+      [final_x_coordinate, final_y_coordinate, final_orientation]
+    end
+  end
+
+  defp final_state([x_coordinate, y_coordinate, orientation], movement_commands) do
+    commands = String.split(movement_commands, "", trim: true)
     Enum.reduce(commands, [x_coordinate, y_coordinate, orientation], fn command, [x_coordinate, y_coordinate, orientation] ->
       case command do
         "L" -> Move.rotate_left([x_coordinate, y_coordinate, orientation])
@@ -9,5 +18,9 @@ defmodule MarsRover do
         "R" -> Move.rotate_right([x_coordinate, y_coordinate, orientation])
       end
     end)
+  end
+
+  defp off_grid?(x_coordinate, y_coordinate, x_axis, y_axis) do
+    x_coordinate < 0 || y_coordinate < 0 || x_coordinate > x_axis || y_coordinate > y_axis
   end
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -3,11 +3,16 @@ defmodule MarsRoverTest do
   doctest MarsRover
 
   test "return a single robots final position and orientation given one command" do
-    assert MarsRover.update_position([1, 1, :south], "L") == [1, 1, :east]
+    assert MarsRover.update_position([1, 1, :south], "L", 2, 2) == [1, 1, :east]
   end
 
   test "return a single robots final position and orientation given multiple commands" do
-    assert MarsRover.update_position([2, 3, :east], "LFRFF") == [4, 4, :east]
-    assert MarsRover.update_position([2, 3, :north], "FLLFR") == [2, 3, :west]
+    assert MarsRover.update_position([2, 3, :east], "LFRFF", 5, 5) == [4, 4, :east]
+    assert MarsRover.update_position([2, 3, :north], "FLLFR", 5, 5) == [2, 3, :west]
+  end
+
+  test "returns 'LOST' if robots final state is off the grid" do
+    assert MarsRover.update_position([0, 2, :north], "FFLFRFF", 4, 8) == "LOST"
+    assert MarsRover.update_position([1, 0, :south], "FFRLF", 4, 8) == "LOST"
   end
 end
