74bb22ce16256f731cda3cb8fafa085736c69795
Himalee <himalee.tailor@gmail.com>
2021-04-24 13:41:06 +0100

    Add method that returns a single robots final state without grid position validation

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,2 +1,13 @@
 defmodule MarsRover do
+
+  def update_position([x_coordinate, y_coordinate, orientation], commands) do
+    commands = String.split(commands, "", trim: true)
+    Enum.reduce(commands, [x_coordinate, y_coordinate, orientation], fn command, [x_coordinate, y_coordinate, orientation] ->
+      case command do
+        "L" -> Move.rotate_left([x_coordinate, y_coordinate, orientation])
+        "F" -> Move.move_forward([x_coordinate, y_coordinate, orientation])
+        "R" -> Move.rotate_right([x_coordinate, y_coordinate, orientation])
+      end
+    end)
+  end
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -2,4 +2,20 @@ defmodule MarsRoverTest do
   use ExUnit.Case
   doctest MarsRover
 
+  test "return a single robots final position and orientation given one command" do
+    assert MarsRover.update_position([1, 1, :south], "L") == [1, 1, :east]
+  end
+
+  test "return a single robots final position and orientation given two commands" do
+    assert MarsRover.update_position([1, 1, :south], "LF") == [2, 1, :east]
+  end
+
+  test "return a single robots final position and orientation given four commands" do
+    assert MarsRover.update_position([1, 1, :north], "FRFL") == [2, 2, :north]
+  end
+
+  test "return a single robots final position and orientation given multiple commands" do
+    assert MarsRover.update_position([2, 3, :east], "LFRFF") == [4, 4, :east]
+    assert MarsRover.update_position([2, 3, :north], "FLLFR") == [2, 3, :west]
+  end
 end
