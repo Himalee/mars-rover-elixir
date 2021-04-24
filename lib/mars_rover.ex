228fbd3e8bb631f9f010fd8065e7ebec4e0f0d89
Himalee <himalee.tailor@gmail.com>
2021-04-24 12:47:38 +0100

    Add method to move robot forward one space

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,18 +1,10 @@
 defmodule MarsRover do
-  @moduledoc """
-  Documentation for `MarsRover`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MarsRover.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def move_forward([x, y, orientation]) do
+    case orientation do
+      :north -> [x, y + 1, orientation]
+      :east -> [x + 1, y, orientation]
+      :west -> [x - 1, y, orientation]
+      :south -> [x, y - 1, orientation]
+    end
   end
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -2,7 +2,19 @@ defmodule MarsRoverTest do
   use ExUnit.Case
   doctest MarsRover
 
-  test "greets the world" do
-    assert MarsRover.hello() == :world
+  test "moves robot forward one space facing north" do
+    assert MarsRover.move_forward([1, 1, :north]) == [1, 2, :north]
+  end
+
+  test "moves robot forward one space facing east" do
+    assert MarsRover.move_forward([1, 1, :east]) == [2, 1, :east]
+  end
+
+  test "moves robot forward one space facing west" do
+    assert MarsRover.move_forward([1, 1, :west]) == [0, 1, :west]
+  end
+
+  test "moves robot forward one space facing south" do
+    assert MarsRover.move_forward([1, 1, :south]) == [1, 0, :south]
   end
 end
