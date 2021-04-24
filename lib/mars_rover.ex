132af1fd2954ea859fde024ee0ece76675969188
Himalee <himalee.tailor@gmail.com>
2021-04-24 17:37:43 +0100

    Returns final positions of multiple robots

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,14 +1,24 @@
 defmodule MarsRover do
 
+  @rotate_left_command "L"
+  @rotate_right_command "R"
+  @move_forward_command "F"
+
+  def final_states(robot_inputs, x_axis, y_axis) do
+    Enum.map(robot_inputs, fn {[x, y, orientation], commands} ->
+      final_state([x, y, orientation], commands, x_axis, y_axis)
+    end)
+  end
+
   def final_state([x, y, orientation], movement_commands, x_axis, y_axis) do
     commands = String.split(movement_commands, "", trim: true)
     initial_result = :not_lost
     Enum.reduce_while(commands, {[x, y, orientation], initial_result}, fn command, {[x, y, orientation], _result} ->
       if is_move_valid?([x, y, orientation], command, x_axis, y_axis) do
         {:cont, case command do
-          "L" -> {Move.rotate_left([x, y, orientation]), :not_lost}
-          "F" -> {Move.move_forward([x, y, orientation]), :not_lost}
-          "R" -> {Move.rotate_right([x, y, orientation]), :not_lost}
+          @rotate_left_command -> {Move.rotate_left([x, y, orientation]), :not_lost}
+          @move_forward_command -> {Move.move_forward([x, y, orientation]), :not_lost}
+          @rotate_right_command -> {Move.rotate_right([x, y, orientation]), :not_lost}
         end
         }
       else
@@ -19,11 +29,11 @@ defmodule MarsRover do
 
   defp is_move_valid?([x, y, orientation], command, x_axis, y_axis) do
     result = case command do
-      "L" -> [next_x, next_y, _] = Move.rotate_left([x, y, orientation])
+      @rotate_left_command -> [next_x, next_y, _] = Move.rotate_left([x, y, orientation])
              result(next_x, next_y, x_axis, y_axis)
-      "F" -> [next_x, next_y, _] = Move.move_forward([x, y, orientation])
+      @move_forward_command -> [next_x, next_y, _] = Move.move_forward([x, y, orientation])
              result(next_x, next_y, x_axis, y_axis)
-      "R" -> [next_x, next_y, _] = Move.rotate_right([x, y, orientation])
+      @rotate_right_command -> [next_x, next_y, _] = Move.rotate_right([x, y, orientation])
              result(next_x, next_y, x_axis, y_axis)
     end
     result == :not_lost
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -11,8 +11,20 @@ defmodule MarsRoverTest do
     assert MarsRover.final_state([2, 3, :north], "FLLFR", 5, 5) == {[2, 3, :west], :not_lost}
   end
 
-  test "returns lost with final position if robots falls off Mars" do
+  test "returns lost with final valid position if robots falls off Mars" do
     assert MarsRover.final_state([0, 2, :north], "FFLFRFF", 4, 8) == {[0, 4, :west], :lost}
     assert MarsRover.final_state([1, 0, :south], "FFRLF", 4, 8) == {[1, 0, :south], :lost}
+  end
+
+  test "returns final states of multiple robots - example 1" do
+    robot_inputs = [{[2, 3, :east], "LFRFF"}, {[0, 2, :north], "FFLFRFF"}]
+    expected_robot_outputs = [{[4, 4, :east], :not_lost}, {[0, 4, :west], :lost}]
+    assert MarsRover.final_states(robot_inputs, 4, 8) == expected_robot_outputs
+  end
+
+  test "returns final states of multiple robots - example 2" do
+    robot_inputs = [{[2, 3, :north], "FLLFR"}, {[1, 0, :south], "FFRLF"}]
+    expected_robot_outputs = [{[2, 3, :west], :not_lost}, {[1, 0, :south], :lost}]
+    assert MarsRover.final_states(robot_inputs, 4, 8) == expected_robot_outputs
   end
 end
